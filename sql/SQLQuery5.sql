377f17205313bb798f355f5a3f6688b37199dbfb
Ammar Mohamed Amin Ahmed ELWazir <ammarwa@gmail.com>
2018-04-17 22:01:30 +0300

    sql

---
+++ sql/SQLQuery5.sql
@@ -0,0 +1,10 @@
+﻿USE [D:\PROJECTS\ELADADELRAKAMY\ELADADELRAKAMY\ADADDATABASE.MDF]
+GO
+
+DECLARE	@return_value Decimal(18, 0)
+
+EXEC	@return_value = [dbo].[getSpentVAT]
+
+SELECT	@return_value as 'Return Value'
+
+GO
